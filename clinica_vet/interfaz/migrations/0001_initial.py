89165399629da32a207a4a40c38c81751af193cd
JUANR18S <riosmesajuancamilo@gmail.com>
2025-07-26 22:16:26 -0500

    Refactor migration models for better readability: format fields in Propietario and Mascota models

--- clinica_vet/interfaz/migrations/0001_initial.py
+++ clinica_vet/interfaz/migrations/0001_initial.py
@@ -15,20 +15,68 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Propietario',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('nombre', models.CharField(max_length=100)),
-                ('telefono', models.CharField(max_length=50)),
-                ('email', models.EmailField(max_length=254)),
+                (
+                    'id',
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID'
+                    )
+                ),
+                (
+                    'nombre',
+                    models.CharField(
+                        max_length=100
+                    )
+                ),
+                (
+                    'telefono',
+                    models.CharField(
+                        max_length=50
+                    )
+                ),
+                (
+                    'email',
+                    models.EmailField(
+                        max_length=254
+                    )
+                ),
             ],
         ),
         migrations.CreateModel(
             name='Mascota',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                (
+                    'id',
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name='ID'
+                    )
+                ),
                 ('nombre', models.CharField(max_length=100)),
-                ('especie', models.CharField(choices=[('Perro', 'Perro'), ('Gato', 'Gato'), ('Otro', 'Otro')], max_length=50)),
+                (
+                    'especie',
+                    models.CharField(
+                        choices=[
+                            ('Perro', 'Perro'),
+                            ('Gato', 'Gato'),
+                            ('Otro', 'Otro')
+                        ],
+                        max_length=50
+                    )
+                ),
                 ('edad', models.PositiveIntegerField()),
-                ('propietario', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='mascotas', to='interfaz.propietario')),
+                (
+                    'propietario',
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name='mascotas',
+                        to='interfaz.propietario'
+                    )
+                ),
             ],
         ),
     ]
